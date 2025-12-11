b8630f012292f7276ed2496c4eee3a7b8d95cfc8
imsinji <57689556+imsinji@users.noreply.github.com>
2025-12-11 17:27:10 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 253.03 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,37 +1,15 @@
-def binary_search_manual(target_list, target_value, n):
-    first = 0
-    last = len(target_list) - 1 - n
-    index = -1
-    
-    while first <= last:
-        midpoint = (first + last) // 2 
-        temp_value = target_list[midpoint+n] - target_list[midpoint]
-
-        if temp_value == target_value:
-            print('found')
-            if index == -1 or index > midpoint:
-                index = midpoint
-                last = midpoint - 1 # 더 작은 인덱스에 데이터가 있는지 확인
-        else:
-            if target_value < temp_value:
-                last = midpoint - 1
-            else:
-                first = midpoint + 1
-    return index
-
-
 def solution(sequence, k):
-    answer = []
-    
-    sum_value = 0
-    prefix_sum = [0]
-    for i in sequence:
-        sum_value += i
-        prefix_sum.append(sum_value)
-
-    for n in range(1, len(sequence)+1):
-        idx = binary_search_manual(prefix_sum, k, n)
-        if idx != -1:
-            return [idx, idx+n-1]
-
-    return answer
+    num = len(sequence)
+    si, ei, s = 0, 0, 0
+    csi, cei = num, 0
+    for i in range(num):
+        ei = i
+        s += sequence[i]
+        while s > k:
+            s -= sequence[si]
+            si += 1
+            if si == ei:
+                break
+        if s == k and (csi > si or (cei - csi ) > (ei - si)):
+            csi, cei = si, ei
+    return [csi, cei]
