d92065933f449c9379011d391cbe5687b73e7a92
imsinji <57689556+imsinji@users.noreply.github.com>
2025-12-11 13:33:14 +0900

    [level 2] Title: 삼각 달팽이, Time: 1905.37 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,20 +1,17 @@
 def solution(n):
     DIRECTIONS = [(1,0), (0,1), (-1,-1)]
     i, j, value, direction = -1, 0, 1, 0
-    a = [[0] * n for _ in range(n)]
+    a = [[0] * i for i in range(1, n+1)]
     total = n * (n + 1) // 2
-
     while value <= total:
         di, dj = DIRECTIONS[direction]
         ni = i + di
         nj = j + dj
-        if ni < nj or not (0 <= ni < n and 0 <= nj < n) or a[ni][nj] != 0:
+        if ni < nj or not (0 <= ni < n and 0 <= nj <= ni) or a[ni][nj] != 0:
             direction = (direction + 1) % len(DIRECTIONS)
-            continue
+        else:
+            a[ni][nj] = value
+            i, j = ni, nj
+            value += 1
 
-        a[ni][nj] = value
-        i, j = ni, nj
-        value += 1
-
-    answer = [value for values in a for value in values if value != 0]
-    return answer
+    return sum(a, [])
