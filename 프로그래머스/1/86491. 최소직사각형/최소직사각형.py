1d83795d022cdde3d5dcbb0fa754ad91fd10f68d
imsinji <57689556+imsinji@users.noreply.github.com>
2025-12-18 13:16:26 +0900

    [level 1] Title: 최소직사각형, Time: 3.68 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,18 +1,4 @@
 def solution(sizes):
-    max_w = 0
-    max_h = 0
-    for size in sizes:
-        w, h = 0, 0
-        if size[0] > size[1]:
-            w = size[0]
-            h = size[1]
-        else:
-            w = size[1]
-            h = size[0]
-        if max_w < w:
-            max_w = w
-        if max_h < h:
-            max_h = h
-
-    answer = max_w * max_h
-    return answer
+    max_w = max(map(lambda x: min(x), sizes))
+    max_h = max(map(lambda x: max(x), sizes))
+    return max_w*max_h
