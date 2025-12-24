f5def16bd3bfeca28434f62a3ed36f1838e8f050
imsinji <57689556+imsinji@users.noreply.github.com>
2025-12-24 14:02:18 +0900

    [level 3] Title: 순위, Time: 4.84 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -2,34 +2,18 @@ from collections import deque
 
 def solution(n, results):
     answer = 0
-    winner = [set() for _ in range(n+1)]
-    looser = [set() for _ in range(n+1)]
+    winners = [set() for _ in range(n+1)]
+    loosers = [set() for _ in range(n+1)]
     for a, b in results:
-        winner[b].add(a)
-        looser[a].add(b)
-
-    w = [set() for _ in range(n+1)]
-    l = [set() for _ in range(n+1)]
+        winners[b].add(a)
+        loosers[a].add(b)
+    
     for i in range(1, n+1):
-        winners = deque(winner[i])
-        visited = set()
-        while winners:
-            target = winners.popleft()
-            if target not in visited:
-                visited.add(target)
-                w[i].add(target)
-                winners.extend(winner[target])
+        for l in loosers[i]: # current에게 진 사람들
+            winners[l].update(winners[i]) # current를 이긴 사람들을 추가
+        for w in winners[i]:
+            loosers[w].update(loosers[i])
 
-        loosers = deque(looser[i])
-        visited.clear()
-        while loosers:
-            target = loosers.popleft()
-            if target not in visited:
-                visited.add(target)
-                l[i].add(target)
-                loosers.extend(looser[target])
-
-        if len(w[i]) + len(l[i]) == n-1:
-            answer += 1
-
+    for i in range(1, n+1):
+        if len(winners[i]) + len(loosers[i]) == n-1: answer += 1
     return answer
