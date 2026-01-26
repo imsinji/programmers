9d7bec59379c7dfba470cfbd4b328006cb8e3b7f
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-27 08:01:30 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -1,16 +1,11 @@
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 vector<int> solution(vector<int> num_list) {
-    int odd_count = 0;
-    int even_count = 0;
-    for (auto n: num_list) {
-        if (n % 2 == 0)
-            even_count++;
-        else
-            odd_count++;
-    }
+    int even_count = std::accumulate(num_list.cbegin(), num_list.cend(), 0, [](int a, int b) { return a + (b%2 == 0 ? 1 : 0); });
+    int odd_count = num_list.size()-even_count;
     return {even_count, odd_count};
 }
