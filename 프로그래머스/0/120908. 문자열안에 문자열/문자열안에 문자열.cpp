ffd6235bc51abb9c6e3f106b3ccf4238caf77049
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-26 15:30:04 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -8,8 +8,6 @@ int solution(string str1, string str2) {
     auto size1 = str1.length();
     auto size2 = str2.length();
     for (int i = 0; i < (size1-size2+1); ++i) {
-        // auto aa = str1.substr(i, size2);
-        // cout << aa << endl;
         if (str2 == str1.substr(i, size2))
             return 1;
     }
