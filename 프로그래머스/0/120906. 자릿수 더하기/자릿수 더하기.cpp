e1398f47d638d78f071b2653df4d9513738d7929
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-27 08:07:13 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    while (true) {
+        answer += n%10;
+        n = n/10;
+        if (n == 0)
+            break;
+    }
+    return answer;
+}
