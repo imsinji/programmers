55dde7ae49033f79166c86758046ac6ac907aac8
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-27 13:03:29 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cpp
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list, int n) {
     vector<int> answer;
-    for (size_t i = 0; i < num_list.size(); i = i+n) {
+    for (size_t i = 0; i < num_list.size(); i += n) {
         answer.emplace_back(num_list[i]);
     }
     return answer;
