78964418bf8022764f39a7c78889f500aa073a7b
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-27 15:55:10 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.03 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cpp
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 vector<int> solution(vector<int> arr, vector<int> delete_list) {
     for (auto del: delete_list) {
-        auto found = std::find(arr.cbegin(), arr.cend(), del);
-        if (found != arr.cend())
-            arr.erase(found);
+        arr.erase(
+            remove_if(
+                arr.begin(), arr.end(),
+                [del](int _) { return _ == del; }
+            ), arr.end()
+        );
     }
     return arr;
 }
