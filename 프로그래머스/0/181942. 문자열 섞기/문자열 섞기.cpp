04875d9fdc5a1ed8a223ae382ab33cbef8d73293
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-28 08:05:22 +0900

    [level 0] Title: 문자열 섞기, Time: 0.03 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string str1, string str2) {
+    string answer = "";
+    if (str1.length() != str2.length())
+        return "";
+    for (size_t i = 0; i < str1.length(); ++i) {
+        answer += str1[i];
+        answer += str2[i];
+    }
+    return answer;
+}
