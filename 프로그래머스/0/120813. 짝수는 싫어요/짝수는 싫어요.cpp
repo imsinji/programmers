57f7333e2a26e8116ba0335c13bdc9fbf9d1f5a4
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-27 16:49:33 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cpp
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 vector<int> solution(int n) {
     vector<int> answer;
-    for (size_t i = 1; i <= n; ++i) {
-        if (i%2 != 0)
-            answer.push_back(i);
+    for (size_t i = 1; i <= n; i += 2) {
+        answer.push_back(i);
     }
     return answer;
 }
