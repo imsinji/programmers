cb4d5fe46954cfa9ab3164f4f8c9f3570982960f
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-28 14:32:43 +0900

    [level 0] Title: 수 조작하기 2, Time: 2.16 ms, Memory: 6.81 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    for (int i = 0; i < numLog.size() -1; i++) {
+        auto diff = numLog[i+1] - numLog[i];
+        if (diff == 1)
+            answer += "w";
+        else if (diff == -1)
+            answer += "s";
+        else if (diff == 10)
+            answer += "d";
+        else if (diff == -10)
+            answer += "a";
+    }
+    return answer;
+}
