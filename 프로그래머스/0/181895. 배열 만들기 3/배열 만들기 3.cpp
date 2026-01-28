92f5e18ea06c118ee14459b60ccfe23ed73cc7c0
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-28 11:30:20 +0900

    [level 0] Title: 배열 만들기 3, Time: 25.21 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.cpp
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.cpp
@@ -6,12 +6,10 @@ using namespace std;
 
 vector<int> solution(vector<int> arr, vector<vector<int>> intervals) {
     vector<int> answer;
-    auto a1 = intervals[0][0];
-    auto b1 = intervals[0][1];
-    auto a2 = intervals[1][0];
-    auto b2 = intervals[1][1];
-    
-    copy(arr.cbegin()+a1, arr.cbegin()+b1+1, std::back_inserter(answer));
-    copy(arr.cbegin()+a2, arr.cbegin()+b2+1, std::back_inserter(answer));
+    for (auto interval: intervals) {
+        for (size_t i = interval[0]; i <= interval[1]; i++) {
+            answer.push_back(arr[i]);
+        }
+    }
     return answer;
 }
