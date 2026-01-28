6871eae42e6e607f3852a98f981bfd0a6015c3c0
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-28 10:02:09 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.10 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    string temp;
+    for (auto s: my_string) {
+        if (s == ' ') {
+            if (temp != "")
+                answer.push_back(temp);
+            temp = "";
+        } else {
+            temp += s;
+        }
+    }
+    if (temp != "")
+        answer.push_back(temp);
+    return answer;
+}
