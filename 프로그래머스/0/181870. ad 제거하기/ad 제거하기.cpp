824bdda8fbb2ccfeed2de184130048a830ad108d
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-28 09:35:22 +0900

    [level 0] Title: ad 제거하기, Time: 0.42 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cpp
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<string> solution(vector<string> strArr) {
     vector<string> answer;
-    for (auto str: strArr) {
+    for (const auto& str: strArr) {
         if (str.find("ad") == std::string::npos)
             answer.push_back(str);
     }
