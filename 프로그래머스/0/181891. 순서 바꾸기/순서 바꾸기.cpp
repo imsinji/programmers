43c74045614b8785ec8430ffc85ed5025ed22f71
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-28 13:25:02 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -5,8 +5,7 @@
 using namespace std;
 
 vector<int> solution(vector<int> num_list, int n) {
-    vector<int> answer;
-    copy(num_list.cbegin()+n, num_list.cend(), std::back_inserter(answer));
+    vector<int> answer = std::vector<int>(num_list.cbegin()+n, num_list.cend());
     copy(num_list.cbegin(), num_list.cbegin()+n, std::back_inserter(answer));
     return answer;
 }
