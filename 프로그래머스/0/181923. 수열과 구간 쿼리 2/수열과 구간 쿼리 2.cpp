3adf7e6c5515ed12e7544db0050d1d818b944183
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-29 08:37:01 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 0.37 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cpp
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cpp
@@ -1,23 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <cmath>
 
 using namespace std;
-
+#define MAX_VAL 999999
 vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
     vector<int> answer;
     for (auto q: queries) {
         auto s = q[0], e = q[1], k = q[2];
-        vector<int> temp(arr.cbegin()+s, arr.cbegin()+e+1);
-        sort(temp.begin(), temp.end());
-        int num = -1;
-        for (auto t: temp) {
-            if (t > k) {
-                num = t;
-                break;
+        int min_val = MAX_VAL;
+        for (int i = s; i <= e; i++) {
+            if (arr[i] > k) {
+                min_val = min(arr[i], min_val);
             }
         }
-        answer.push_back(num);
+        if (min_val == MAX_VAL) 
+            min_val = -1;
+        answer.push_back(min_val);
     }
     return answer;
 }
