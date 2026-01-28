177d3e199e07082995cb852af96aa510df0cb310
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-28 14:54:27 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 2.09 ms, Memory: 5.94 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -4,11 +4,9 @@
 using namespace std;
 
 vector<vector<int>> solution(int n) {
-    vector<int> comp(n, 0);
-    vector<vector<int>> answer{};
+    vector<vector<int>> answer(n, vector<int>(n, 0));
     for (int i = 0; i < n; i++) {
-        answer.emplace_back(comp);
-        answer.back()[i] = 1;
+        answer[i][i] = 1;
     }
     return answer;
 }
