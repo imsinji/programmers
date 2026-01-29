62599c15f00a816589ecd20964293d1812d30bad
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-29 10:38:42 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 35.99 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.cpp
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 int solution(vector<int> arr) {
-    int x = 0;
-    while (true) {
-        auto temp = arr;
+    int x = -1;
+    int count = -1;
+    while (count != 0) {
+        count = 0;
         for (auto& a: arr) {
             if (a>=50 && a%2==0) {
                 a /= 2;
+                count++;
             } else if (a <50 && a%2==1) {
                 a = (a*2 + 1);
+                count++;
             }
-        }
-        if (arr == temp) {
-            return x;
         }
         x++;
     }
-    return -1;
+    return x;
 }
