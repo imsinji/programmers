e6d389321c45058559a8f23f7dc7685b410c1143
imsinji <57689556+imsinji@users.noreply.github.com>
2026-01-30 09:15:54 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cpp
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <vector>
+#include <queue>
+
 #include <algorithm>
 #include <cmath>
 #include <iostream>
@@ -8,26 +10,16 @@ using namespace std;
 
 vector<int> solution(int l, int r) {
     vector<int> answer;
-    auto max_i = 0;
-    while (r > pow(10, max_i)) max_i++;
-    
-    vector<vector<int>> cal = {{5}};
-    cal.resize(max_i);
-    for (int i = 1; i < max_i; i++) {
-        vector<int> temp;
-        for (auto c: cal[i-1]) {
-            temp.push_back(c*10);
-            temp.push_back(c*10+5);
-        }
-        cal[i] = temp;
-    }
-    
-    for (auto c: cal) {
-        for (auto cc: c) {
-            if (cc >= l && cc <= r) {
-                answer.push_back(cc);
-            }
-        }
+    queue<int> q;
+    q.push(5);
+    while (!q.empty()) {
+        int n = q.front();
+        q.pop();
+        if (n > r) break;
+        if (n >= l && n <= r)
+            answer.push_back(n);
+        q.push(n*10);
+        q.push(n*10+5);
     }
     if (answer.empty())
         return {-1};
